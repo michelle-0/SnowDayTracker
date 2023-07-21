df50b44cd19e080b39b873751e346d8b041aeb20
michelle-0 <michelle.hung.12@gmail.com>
2023-07-20 17:11:29 -0700

    set up navigation with tabs

--- App.js
+++ App.js
@@ -2,18 +2,22 @@ import React from 'react'
 import { StyleSheet, Text, View, SafeAreaView } from 'react-native'
 import UpcomingWeather from './src/screens/UpcomingWeather'
 import City from './src/screens/City'
+import CurrentWeather from './src/screens/CurrentWeather'
+import { NavigationContainer } from '@react-navigation/native'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+
+const Tab = createBottomTabNavigator() // initializes tab object for us to use
 
 const App = () => {
   return (
-    <View style={styles.container}>
-      <City/>
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name={'Current'} component={CurrentWeather}/>
+        <Tab.Screen name={'Upcoming'} component={UpcomingWeather}/>
+        <Tab.Screen name={'City'} component={City} />
+      </Tab.Navigator>
+    </NavigationContainer>
   )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1
-  }
-})
 export default App
