06703909fdf521fe57cdd368a653d05e8bf88e31
michelle-0 <michelle.hung.12@gmail.com>
2023-08-14 17:16:22 -0700

    set up push request to AWS lambda

--- src/screens/Counter.js
+++ src/screens/Counter.js
@@ -2,58 +2,45 @@ import React, { useEffect, useState } from 'react';
 import { View, Text, FlatList, StyleSheet, StatusBar, TouchableOpacity } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import Card from '../components/Card';
-import ListItem from "../components/ListItem";
-import DB from "../components/DB";
-import { Button } from 'react-native-web';
 import LastCard from '../components/LastCard';
 const Counter = ({}) => {const { container, image, button, lastCard } = styles
 const [data, setData] = useState([]);
 
 useEffect(() => {
-    // Define the API URL
-    const apiUrl = 'https://p92mbzda54.execute-api.us-west-2.amazonaws.com/beta/all';
+    const apiUrl = 'https://b3y4z9h2hb.execute-api.us-west-2.amazonaws.com/snowdays/mo@gmail.com';
 
-    // Make the API call using fetch()
     fetch(apiUrl)
         .then(response => response.json())
         .then(data => {
-            // Handle the successful response
-            setData(data.Items);
+            setData(data);
         })
         .catch(error => {
-            // Handle any errors that occurred during the API call
             console.error('Error fetching data:', error);
         });
 }, []);
 
-// Render each item in the FlatList
 const renderDaysItem = ({ item, index }) => {
-    // Check if it's the last item
-    const isLastItem = index === data.length;
-  
+    const isLastItem = index === data.length; 
+    const { Snowy, Days } = item; 
+
     if (isLastItem) {
       return (
         <View>
-        <LastCard
-        />
-
+          <LastCard />
         </View>
       );
     }
-  
-    const snowy = item.Snowy?.S;
-    const days = item.Days?.N;
-  
+
     return (
       <Card
-        title={`${snowy}`}
-        content={`Days: ${days}`} 
+        title={Snowy}
+        content={`Days: ${Days}`} 
       />
     );
   };
 
 return (
-    <SafeAreaView edges={["top"]} style={container}>
+    <SafeAreaView style={container}>
         <View>
             <FlatList
                 data={[...data, {}]}
@@ -61,7 +48,6 @@ return (
                 keyExtractor={(item, index) => index.toString()}
             />
         </View>
-        <TouchableOpacity style={button}><Text>ADD MOUNTAIN</Text></TouchableOpacity>
     </SafeAreaView>
 );
 };
